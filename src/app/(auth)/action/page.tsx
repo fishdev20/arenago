7c4a9b34d1650e358bc53a7ff39bb70cc50bcc41
Marcus <minh.nguyen@triona.fi>
2025-11-09 21:48:24 +0200

    fix param

--- src/app/(auth)/action/page.tsx
+++ src/app/(auth)/action/page.tsx
@@ -1,32 +1,36 @@
-'use client'
+// 'use client'
 
-import { Spinner } from '@/components/ui/spinner'
-import { useSearchParams } from 'next/navigation'
-import { Suspense } from 'react'
-// import ActionRouter from './action-router'
+// import { Spinner } from '@/components/ui/spinner'
+// import { useSearchParams } from 'next/navigation'
+// import { Suspense } from 'react'
+// // import ActionRouter from './action-router'
 
-export default function FirebaseActionPage() {
-  const searchParams = useSearchParams()
-  const mode = searchParams.get('mode')
-  const oobCode = searchParams.get('oobCode')
+// export default function FirebaseActionPage() {
+//   const searchParams = useSearchParams()
+//   const mode = searchParams.get('mode')
+//   const oobCode = searchParams.get('oobCode')
 
-  if (!mode || !oobCode) {
-    return (
-      <div className="flex items-center justify-center min-h-screen text-center">
-        <p>Invalid or missing action link.</p>
-      </div>
-    )
-  }
+//   if (!mode || !oobCode) {
+//     return (
+//       <div className="flex items-center justify-center min-h-screen text-center">
+//         <p>Invalid or missing action link.</p>
+//       </div>
+//     )
+//   }
 
-  return (
-    <Suspense
-      fallback={
-        <div className="flex items-center justify-center min-h-screen">
-          <Spinner /> Processing...
-        </div>
-      }
-    >
-      {/* <ActionRouter mode={mode} oobCode={oobCode} /> */}
-    </Suspense>
-  )
+//   return (
+//     <Suspense
+//       fallback={
+//         <div className="flex items-center justify-center min-h-screen">
+//           <Spinner /> Processing...
+//         </div>
+//       }
+//     >
+//       {/* <ActionRouter mode={mode} oobCode={oobCode} /> */}
+//     </Suspense>
+//   )
+// }
+
+export default function Page() {
+  return <>Test</>
 }
